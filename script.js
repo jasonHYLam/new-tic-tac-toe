f6d375e5506ab1217cf2e766ea78b40257935aec
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-06-28 10:08:53 +0100

    Separate cell logic from board logic
    Create module called cellLogic that gets cell and changes cell.

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
+// gameboard logic. It should get the board, change the board, and display the board.
 const gameBoard = (() => {
     const rows = 3;
     const columns = 3;
     const board = [];
     
     for (let i = 0; i < rows; i++) {
-        console.log('x')
         let row = [];
         for (let j = 0; j < columns; j++) {
-            row.push('x');
+            row.push(cellLogic.getCell());
         }
         board.push(row);
     }
     console.log(board)
-})
+
+    function getBoard() {
+        return board;
+    }
+
+    // put a piece anywhere that is currently empty. when placing a piece, modify that tile.
+    function addPiece() {
+
+
+    }
+
+    function displayBoard() {
+
+    }
+
+});
+
+// change cell contents and get current cell
+const cellLogic = (() => {
+    let cell = 'y';
+
+    const getCell = () => cell;
+
+    // obtain player marker
+    const changeCell = {
+        // cell = playerMarker;
+    }
+    return {getCell, changeCell};
+})();
 
 gameBoard();
